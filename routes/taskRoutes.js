793c4f2b7bf77046827e997727ba355b83db709a
ponnnarasi14 <ponnarasi14@gmail.com>
2025-04-26 11:46:07 +0530

    joi validator

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const joi = require('joi');
 const router = express.Router();
 const Task = require('../models/Task');
 
+//define joi schema
+const taskSchema = joi.object({
+
+    title: joi.string().required().messages({
+        
+        'string.empty' : 'Title is Required'
+    }),
+    completed: joi.boolean().optional().messages({
+
+        'boolean.base' : 'Completed must be true or false'
+    })
+
+}).unknown(true);
+
 //add task
 router.post('/tasks', async (req, res) => {
+
+    const { error, value } = taskSchema.validate(req.body);
+
+    if(error) { return res.status(400).json({ status: false, message: error.details[0].message }); }
+
     try{
-        const task = await Task.create(req.body);
+        const task = await Task.create(value); // use validated `value` instead of raw req.body
+
         res.status(201).json(task);
     }
     catch (err){
+
         res.status(400).json({ error: err.message });
     }
 
@@ -17,25 +39,43 @@ router.post('/tasks', async (req, res) => {
 //get all task
 router.get('/tasks', async(req, res) => {
     const tasks = await Task.find();
-    res.json(tasks);
+    res.status(200).json(tasks);
 })
 
 //update task
 router.put('/tasks/:id', async(req, res) => {
+
+    const { error, value } = taskSchema.validate(req.body);
+
+    if(error) { return res.status(400).json({ status: false, message: error.details[0].message }); }
+
     try{
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        const task = await Task.findByIdAndUpdate(req.params.id, value, {new: true});
+
         if(!task) return res.status(404).json({ message: 'Task not found' });
-        res.json({ message: 'Task Updated', task});
+
+        res.status(200).json({ message: 'Task Updated', task});
     }
     catch (err){
-        res.status(500).json({ error: 'Something Went Wrong' });
+
+        res.status(500).json({ error: 'Internal server error' });
     }
 });
 
 //delete task
 router.delete('/tasks/:id', async(req, res) => { 
-    await Task.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Task Deleted' })
+    try 
+    {
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if(!task) { return res.status(404).json({ message: 'Task Not Found' }); }
+
+        res.status(200).json({ message: 'Task Deleted' })
+
+    } catch (err) { 
+
+        res.status(500).json({ error: 'Internal server error' }); 
+    }
 })
 
 module.exports = router;
